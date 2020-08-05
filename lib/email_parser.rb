2d27f7fdf605050e08a448ca9fbdb27ab7719f66
Tiffani Best <tiffanimelissabest@gmail.com>
2020-08-05 02:40:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,1 +1,15 @@
+class EmailParser 
+  attr_accessor :name, :csv_emails
+  
+  def initialize(csv_emails)
 
+    @csv_emails = csv_emails
+  end 
+
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end
