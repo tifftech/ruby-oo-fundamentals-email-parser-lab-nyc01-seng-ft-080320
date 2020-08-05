a306c8b5dd923169f6d53c855e715e0d9e794478
Tiffani Best <tiffanimelissabest@gmail.com>
2020-08-05 02:43:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,21 @@
 class EmailParser
-	attr_accessor :emails
-	
-	def initialize(emails)
-		@emails= emails
-	end
-	
-	def parse
-		@emails = @emails.gsub(/[\s,]/ ," ").split
-		@emails.uniq
-	end
-	
+
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    email_array = []
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
+  end
+
 end
+
+emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
+parser = EmailParser.new(emails)
+
+puts parser.parse
